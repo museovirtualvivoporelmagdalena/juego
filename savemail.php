140fee0febc0349b581af3755d16c0e73a86dad9
museovirtualvivoporelmagdalena <museovirtualvivoporelmagdalena@gmail.com>
2024-10-02 07:00:19 -0500

    Add files via upload

---
+++ savemail.php
@@ -0,0 +1,23 @@
+<?php 
+
+	$name = $_POST['name'];
+	$timep = $_POST['score'];
+
+	$xml = new DomDocument('1.0','UTF-8');
+	$xml->load('reg.xml');
+	
+	$rootTag = $xml->getElementsByTagName('root')->item(0);
+	
+	$scoreTag = $xml->createElement('score');
+	
+	$nameTag = $xml->createElement('name',$name);
+	$timeTag = $xml->createElement('timep',$timep);
+	
+	$scoreTag->appendChild($nameTag);
+	$scoreTag->appendChild($timeTag);
+	
+	$rootTag->appendChild($scoreTag);
+	
+	$xml->save('reg.xml');
+
+?>
